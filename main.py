2c4ae1e71b9c186b1d43d5d9c794e9d9c864983c
slzhang13 <slzhang13@qq.com>
2023-04-09 21:39:00 +0800

    upload

---
+++ main.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import time
+from util import *
+
+
+if __name__ == "__main__":
+
+    douban_group = "beijingzufang"
+
+    print("=========================================================")
+    print("开始执行，时间：", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n")
+
+    ## 连接数据库、创建数据表
+    rent_info = sqlite3.connect("rent_info.db")
+    rent_info.execute(
+        f"""CREATE TABLE IF NOT EXISTS {douban_group}
+                (title TEXT NOT NULL,
+                update_time TEXT NOT NULL,
+                link TEXT NOT NULL);"""
+    )
+
+    print("数据库连接成功，数据表创建成功\n")
+
+    ## 读取上次执行添加的最新一条数据
+    cursor = rent_info.cursor()
+    cursor.execute(f"SELECT * FROM {douban_group} ORDER BY update_time DESC")
+    last_update = (
+        cursor.fetchone()[1]
+        if cursor.fetchone()
+        else time.strftime(
+            "%m-%d %H:%M", time.localtime(time.time() - 3600 * 4)
+        )  # 当数据表是初次建立时，从4小时前开始爬取
+    )
+
+    ## 一直爬取到上次执行时的最新更新时间
+    max_page = 20  # 每次爬取的最大页数（6h执行一次，20页足够包含6h内的更新）
+    exit_flag = False
+    latest_update = -1
+    num = 0
+
+    print("开始爬取...")
+
+    for page in range(1, max_page + 1):  # 从最新页开始爬取
+
+        time.sleep(5)  # 爬取不要太快，防止被封
+
+        # print(f"\t正在爬取第{page}页...")
+
+        page_info = crawl_page(douban_group, page)  # 爬取一页
+
+        if page == 1:
+            latest_update = page_info[0][1]
+
+        for item in page_info:  # 遍历当前页中的每一条信息
+
+            if last_update > item[1]:  # 已经爬取到上次执行时的最新更新时间，退出
+                exit_flag = True
+                break
+            else:
+                num += 1
+                # 提供link查询当前信息是否已经存在，存在则更新，不存在则插入
+                cursor.execute(f"SELECT * FROM {douban_group} WHERE link=?", (item[2],))
+                row = cursor.fetchone()
+                if row:  # 存在则更新
+                    cursor.execute(
+                        f"UPDATE {douban_group} SET title=?, update_time=? WHERE link=?",
+                        (item[0], item[1], item[2]),
+                    )
+                else:  # 不存在则插入
+                    cursor.execute(
+                        f"INSERT INTO {douban_group} VALUES (?, ?, ?)",
+                        (item[0], item[1], item[2]),
+                    )
+        if exit_flag:
+            break
+
+    rent_info.commit()
+    cursor.close()
+    rent_info.close()
+
+    print("\n本次爬取结束，共爬取", page, "页", num, "条信息\n")
+    print(f"从{last_update}更新至{latest_update}\n")
+
+    db_name = "rent_info.db"  # 数据库名
+    table_name = "beijingzufang"  # 豆瓣小组
+    xls_write(db_name, table_name)
+
+    print("数据筛选并导出成功\n")
+    print("执行完毕，时间：", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print("=========================================================\n")
---
+++ util.py
@@ -0,0 +1,85 @@
+import re
+import os
+import sqlite3
+import requests
+import xlsxwriter as xw
+
+# 爬取豆瓣小组某一页的信息
+def crawl_page(douban_group, page):
+
+    url = f"https://www.douban.com/group/{douban_group}/discussion?start={25*(page-1)}&type=new"
+
+    cookie = os.environ.get("COOKIE")  # get from repository secrets
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
+        "Cookie": cookie,
+    }
+
+    # raw text
+    html_raw = requests.get(url, headers=headers).text
+
+    # raw items
+    items_raw = re.findall(r"<tr class=\"\">([\s\S]*?)<\/tr>", html_raw)
+
+    page_info = []
+    for i in range(len(items_raw)):
+        item = items_raw[i]
+        item = re.sub(" +", " ", item.replace("\n", " "))
+        link_title = re.findall(r"<a href=\"(.*?)title=\"(.*?)\"", item)
+        update = re.findall(r"class=\"time\">(.*?)<\/td>", item)
+        page_info.append([link_title[0][1], update[0], link_title[0][0]])
+
+    return page_info
+
+
+# 读取数据表内容，且按照update_time排序
+def db_read(db_name, table_name, order_by="update_time", order="DESC"):
+
+    db = sqlite3.connect(db_name)
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM {table_name} ORDER BY {order_by} {order}")
+    rows = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    return rows
+
+
+# 按关键字对标题筛选
+def filt_item(item):
+    # need further definition
+    return True
+
+
+def xls_write(db_name, table_name):
+
+    ## 读取数据库
+    rows = db_read(db_name, table_name)  # 已排序
+
+    ## 筛选并写入xlsx
+    filename = f"rent_info_{table_name}.xlsx"
+    workbook = xw.Workbook(filename)
+    worksheet = workbook.add_worksheet("sheet1")
+    worksheet.activate()
+    # 按照实际格式调整
+    worksheet.set_column("A:A", 60)
+    worksheet.set_column("B:B", 30)
+    worksheet.set_column("C:C", 50)
+    sheet_title = ["标题", "最新回复时间", "链接"]
+
+    worksheet.write_row("A1", sheet_title)
+
+    for i, row in enumerate(rows):
+        title = row[0]
+        if filt_item(title):
+            idx = "A" + str(i + 2)
+            worksheet.write_row(idx, row)
+
+    workbook.close()
+
+
+if __name__ == "__main__":
+
+    db_name = "rent_info.db"
+    table_name = "beijingzufang"  # 豆瓣小组
+    xls_write(db_name, table_name)
